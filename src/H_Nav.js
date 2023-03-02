ea5e7abb0919e23ff42b45975d0a1a9aceca06d0
jaemin96 <lemon__96@naver.com>
2023-03-03 00:34:39 +0900

    Style: 헤더 스타일 수정

--- src/H_Nav.js
+++ src/H_Nav.js
@@ -10,49 +10,51 @@ import * as ReactBootStrap from "react-bootstrap";
 //$ npm install --save react-navbar-dropdown
 //npm install react-bootstrap bootstrap
 class J_Nav extends React.Component {
+  // constructor(props) {
+  //   this.state = {
+  //     show: false,
+  //   };
+  //   this.showDropdown = this.showDropdown.bind(this);
+  //   this.hideDropdown = this.hideDropdown.bind(this);
+  // }
+  // showDropdown() {
+  //   this.setState({ show: true });
+  // }
+  // hideDropdown() {
+  //   this.setState({ show: false });
+  // }
   render() {
     return (
       <div className="nav_Background">
         {/**   <!-- Navbar   --> */}
         <ReactBootStrap.Navbar bg="light" variant="light" expand="lg">
-          <ReactBootStrap.Navbar.Brand href="#home">
-            <Link to="/" className="Nav_title">
-              Dawntech
-            </Link>
-          </ReactBootStrap.Navbar.Brand>
-
+          Dawntech
           <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+          <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav ">
             <ReactBootStrap.Nav className="mr-auto">
-              <ReactBootStrap.Nav.Link href="#home" className="navheader_title">
-                <Link to="/" className="Link_btn">
-                  HOME
+              <ReactBootStrap.Nav.Link href="/" className="nav_home">
+                <Link to="/" className="nav_home_btn">
+                  홈
                 </Link>
               </ReactBootStrap.Nav.Link>
 
               <ReactBootStrap.NavDropdown
                 title="회사소개"
                 id="basic-nav-dropdown"
-                className="navheader_title"
+                className="navheader_title "
               >
-                {/* 
-                    <ReactBootStrap.NavDropdown.Item >
-                         <Link to="/Company_Guide/Company_Introduce" className ="Link_btn " >연혁</Link>
-                    </ReactBootStrap.NavDropdown.Item>
-                    */}
-                <ReactBootStrap.NavDropdown.Item>
-                  <Link
-                    to="/Company_Guide/Company_Introduce"
-                    className="Link_btn "
-                  >
-                    인사말
-                  </Link>
+                <ReactBootStrap.NavDropdown.Item
+                  href={`/Company_Guide/Company_Introduce`}
+                  className="Link_btn "
+                >
+                  인사말
                 </ReactBootStrap.NavDropdown.Item>
 
-                <ReactBootStrap.NavDropdown.Item>
-                  <Link to="/Company_Guide/Company_Come" className="Link_btn ">
-                    오시는 길
-                  </Link>
+                <ReactBootStrap.NavDropdown.Item
+                  href={`/Company_Guide/Company_Come`}
+                  className="Link_btn "
+                >
+                  오시는 길
                 </ReactBootStrap.NavDropdown.Item>
               </ReactBootStrap.NavDropdown>
 
@@ -69,19 +71,18 @@ class J_Nav extends React.Component {
                 id="basic-nav-dropdown"
                 className="navheader_title"
               >
-                <ReactBootStrap.NavDropdown.Item>
-                  <Link to="/Company_Tech" className="Link_btn ">
-                    기술 자료실
-                  </Link>
+                <ReactBootStrap.NavDropdown.Item
+                  href={`/Company_Tech`}
+                  className="Link_btn "
+                >
+                  기술 자료실
                 </ReactBootStrap.NavDropdown.Item>
 
-                <ReactBootStrap.NavDropdown.Item>
-                  <Link
-                    to="/Company_Tech/Company_Variety_Tech"
-                    className="Link_btn "
-                  >
-                    TECH
-                  </Link>
+                <ReactBootStrap.NavDropdown.Item
+                  href={`/Company_Tech/Company_Variety_Tech`}
+                  className="Link_btn "
+                >
+                  TECH
                 </ReactBootStrap.NavDropdown.Item>
               </ReactBootStrap.NavDropdown>
 
@@ -90,10 +91,11 @@ class J_Nav extends React.Component {
                 id="basic-nav-dropdown"
                 className="navheader_title"
               >
-                <ReactBootStrap.NavDropdown.Item>
-                  <Link to="/Notice" className="Link_btn ">
-                    공지사항
-                  </Link>
+                <ReactBootStrap.NavDropdown.Item
+                  href={`/Notice`}
+                  className="Link_btn "
+                >
+                  공지사항
                 </ReactBootStrap.NavDropdown.Item>
               </ReactBootStrap.NavDropdown>
 
@@ -107,25 +109,25 @@ class J_Nav extends React.Component {
                          <Link to="/Customer/Customer_Guide" className ="Link_btn " >고객센터 안내</Link>
                     </ReactBootStrap.NavDropdown.Item>
                     */}
-                <ReactBootStrap.NavDropdown.Item>
-                  <Link to="/Customer/Customer_Detail" className="Link_btn ">
-                    문의하기
-                  </Link>
+                <ReactBootStrap.NavDropdown.Item
+                  href={`/Customer/Customer_Detail`}
+                  className="Link_btn "
+                >
+                  문의하기
                 </ReactBootStrap.NavDropdown.Item>
 
-                <ReactBootStrap.NavDropdown.Item>
-                  <Link to="/Customer" className="Link_btn ">
-                    답변확인
-                  </Link>
+                <ReactBootStrap.NavDropdown.Item
+                  href={`/Customer`}
+                  className="Link_btn "
+                >
+                  답변확인
                 </ReactBootStrap.NavDropdown.Item>
 
-                <ReactBootStrap.NavDropdown.Item>
-                  <Link
-                    to="/Customer/Customer_Board_list"
-                    className="Link_btn "
-                  >
-                    고객 게시판
-                  </Link>
+                <ReactBootStrap.NavDropdown.Item
+                  vhref={`/Customer/Customer_Board_list`}
+                  className="Link_btn "
+                >
+                  고객 게시판
                 </ReactBootStrap.NavDropdown.Item>
               </ReactBootStrap.NavDropdown>
             </ReactBootStrap.Nav>
